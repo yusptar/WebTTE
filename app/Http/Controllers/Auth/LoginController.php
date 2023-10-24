e8f46f4da8d2141e6580f058cbff74403bf830e2
yusptar <yusupvatar@gmail.com>
2023-10-24 13:46:58 +0700

    add backup login function tanpa NIK

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -15,24 +15,37 @@ class LoginController extends Controller
 {
     use AuthenticatesUsers;
 
-    public function login(Request $request)
-    {
+
+    public function login(Request $request){
         $credentials = $request->only('username', 'password');
-        // Authenticate the user
-        if (auth()->attempt($credentials)) {
-            $user_data = Pegawai::where('nik', $credentials['username'])->first(); // Assuming 'username' is the NIK
-            $ses_data = [
-                'id' => $user_data->nik, // Assuming 'nik' is the user ID
-                'username' => $user_data->nama, // Assuming 'nama' is the username
-            ];
-            Session::put($ses_data);
+
+        if(auth()->attempt($credentials)){
             Alert::success('Login Berhasil!', 'Selamat Datang');
             return redirect()->route('dashboard');
-        } else {
+        }else{
             Alert::error('Oops! Login Gagal.', 'Terdapat Kesalahan!');
             return redirect()->back();
-        }             
+        }
     }
+
+    // public function login(Request $request)
+    // {
+    //     $credentials = $request->only('username', 'password');
+    //     // Authenticate the user
+    //     if (auth()->attempt($credentials)) {
+    //         $user_data = Pegawai::where('nik', $credentials['username'])->first(); // Assuming 'username' is the NIK
+    //         $ses_data = [
+    //             'id' => $user_data->nik, // Assuming 'nik' is the user ID
+    //             'username' => $user_data->nama, // Assuming 'nama' is the username
+    //         ];
+    //         Session::put($ses_data);
+    //         Alert::success('Login Berhasil!', 'Selamat Datang');
+    //         return redirect()->route('dashboard');
+    //     } else {
+    //         Alert::error('Oops! Login Gagal.', 'Terdapat Kesalahan!');
+    //         return redirect()->back();
+    //     }             
+    // }
     
     public function __construct()
     {
