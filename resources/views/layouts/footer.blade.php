2e60b082ae91f94367b01eda43a552b8211e9e83
yusptar <yusupvatar@gmail.com>
2024-01-15 14:18:09 +0700

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
     <strong>Copyright &copy; 2023 <a href="">IT RST</a>.</strong>
     All rights reserved.  
-    <img src="{{ asset('img/logo-bsre.png') }}" alt="" width="6%" height="3%">
+    <a href="https://bsre.bssn.go.id/" target="_blank"><img src="{{ asset('img/logo-bsre.png') }}" alt="" width="6%" height="3%"></a>
     
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 1.0
