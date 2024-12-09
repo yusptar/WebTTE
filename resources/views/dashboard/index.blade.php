e2c0c726dba819e73a97cc64b4fbeee02dda191a
yusptar <yusupvatar@gmail.com>
2024-12-09 12:39:29 +0700

    fixing dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -17,8 +17,35 @@
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
+            <div class="row">
+                <!-- <div class="col-lg-6 col-6">
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{ $jumlah_tte->sudah }}</h3>
+                            <h6>Jumlah TTE <strong>{{ Auth::user()->pegawai->nama }}</strong> berstatus (SUDAH)</h6>
+                        </div>
+                        <div class="icon">
+                          <i class="fas fa-check"></i>
+                        </div>
+                    </div>
+                </div> -->
+                <!-- <div class="col-lg-6 col-6">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{ $jumlah_tte->belum }}</h3>
+                            <h6>Jumlah TTE <strong>{{ Auth::user()->pegawai->nama }}</strong> berstatus (BELUM)</h6>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-minus"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">
+                          
+                        </a>
+                    </div>
+                </div> -->
+            </div>
             <div class="col-md-12 mr-10 text-center">
-                <img src="{{ asset('img/alur-tte-new.png') }}" alt="" class="" width="80%" height="50%">
+                <img src="{{ asset('img/alur-tte-new.png') }}" alt="" class="" width="70%" height="30%">
             </div>
         </div><!-- /.container-fluid -->
     </section>
