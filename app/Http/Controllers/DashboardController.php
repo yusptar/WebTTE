dd2370dd042a1b3f18c6263a76e84f2fca1b1a15
yusptar <yusupvatar@gmail.com>
2024-12-10 14:04:03 +0700

    update dashboard (jumlah penggunaan TTE)

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,26 +6,72 @@ use Illuminate\Http\Request;
 use App\Models\Pegawai; 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 
 class DashboardController extends Controller
 {
-    
+    // public function index()
+    // {
+    //     $jumlah_tte = DB::table('pegawai as p')
+    //         ->join('status_tte_ppa as s', 'p.nik', '=', 's.nip')
+    //         ->join('master_berkas_digital as m', 's.jenis_rm', '=', 'm.kode')
+    //         ->join('manajemen_rm_tte as r', 'r.no_rawat', '=', 's.no_rawat')
+    //         ->select(
+    //             'p.nama', 
+    //             's.nip', 
+    //             DB::raw('SUM(CASE WHEN s.status = "BELUM" THEN 1 ELSE 0 END) AS belum'),
+    //             DB::raw('SUM(CASE WHEN s.status = "SUDAH" THEN 1 ELSE 0 END) AS sudah')
+    //         )
+    //         ->where('p.nama', Auth::user()->pegawai->nama) 
+    //         ->first();
+
+    //     return view('dashboard.index', compact('jumlah_tte'));
+    // }
+
     public function index()
     {
+        $user_name = Auth::user()->pegawai->nama;
+
+        // Jumlah TTE per bulan
+        $tte_per_month = DB::table('status_tte_ppa as s')
+            ->join('pegawai as p', 's.nip', '=', 'p.nik')
+            ->join('manajemen_rm_tte as r', 'r.no_rawat', '=', 's.no_rawat')
+            ->select(
+                DB::raw('MONTH(r.tanggal_upload) as month'),
+                DB::raw('SUM(CASE WHEN s.status = "BELUM" THEN 1 ELSE 0 END) as belum'),
+                DB::raw('SUM(CASE WHEN s.status = "SUDAH" THEN 1 ELSE 0 END) as sudah')
+            )
+            ->where('p.nama', $user_name)
+            ->groupBy(DB::raw('MONTH(r.tanggal_upload)'))
+            ->orderBy(DB::raw('MONTH(r.tanggal_upload)'))
+            ->get();
+
+        // Format data untuk grafik
+        $months = [];
+        $belum = [];
+        $sudah = [];
+        foreach ($tte_per_month as $data) {
+            $months[] = Carbon::create()->month($data->month)->format('F'); // Nama bulan
+            $belum[] = $data->belum;
+            $sudah[] = $data->sudah;
+        }
+
+      
         $jumlah_tte = DB::table('pegawai as p')
             ->join('status_tte_ppa as s', 'p.nik', '=', 's.nip')
             ->join('master_berkas_digital as m', 's.jenis_rm', '=', 'm.kode')
             ->join('manajemen_rm_tte as r', 'r.no_rawat', '=', 's.no_rawat')
             ->select(
-                DB::raw('SUM(CASE WHEN s.status = "BELUM" THEN 1 ELSE 0 END) AS belum'),
-                DB::raw('SUM(CASE WHEN s.status = "SUDAH" THEN 1 ELSE 0 END) AS sudah')
+                DB::raw('SUM(CASE WHEN s.status = "BELUM" THEN 1 ELSE 0 END) as belum'),
+                DB::raw('SUM(CASE WHEN s.status = "SUDAH" THEN 1 ELSE 0 END) as sudah')
             )
-            ->where('p.nama', Auth::user()->name)
+            ->where('p.nama', $user_name)
             ->first();
 
-        return view('dashboard.index', compact('jumlah_tte'));
+        return view('dashboard.index', compact('jumlah_tte', 'months', 'belum', 'sudah'));
     }
+
 
     public function __construct()
     {
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,7 +7,7 @@
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1 class="m-0" style="font-weight:bold">Dashboard</h1>
-                    <h6 class="m-0" style="font-weight:bold">Alur Penggunaan Aplikasi TTE</h6>
+                    <h6 class="m-0" style="font-weight:bold">Detail Penggunaan Aplikasi TTE</h6>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -18,38 +18,172 @@
     <section class="content">
         <div class="container-fluid">
             <div class="row">
-                <!-- <div class="col-lg-6 col-6">
+                <div class="col-lg-6 col-6">
                     <div class="small-box bg-success">
                         <div class="inner">
-                            <h3>{{ $jumlah_tte->sudah }}</h3>
-                            <h6>Jumlah TTE <strong>{{ Auth::user()->pegawai->nama }}</strong> berstatus (SUDAH)</h6>
+                            <h3>{{ $jumlah_tte->sudah }} Dokumen</h3>
+                            <h6>Jumlah Keseluruhan Dokumen TTE berstatus <strong style="color:white; font-weight:bold">(SUDAH)</strong></h6>
                         </div>
                         <div class="icon">
                           <i class="fas fa-check"></i>
                         </div>
                     </div>
-                </div> -->
-                <!-- <div class="col-lg-6 col-6">
+                </div>
+                <div class="col-lg-6 col-6">
                     <div class="small-box bg-info">
                         <div class="inner">
-                            <h3>{{ $jumlah_tte->belum }}</h3>
-                            <h6>Jumlah TTE <strong>{{ Auth::user()->pegawai->nama }}</strong> berstatus (BELUM)</h6>
+                            <h3>{{ $jumlah_tte->belum }} Dokumen</h3>
+                            <h6>Jumlah Keselurahan Dokumen TTE berstatus <strong style="color:white; font-weight:bold">(BELUM)</strong></h6>
                         </div>
                         <div class="icon">
                             <i class="fas fa-minus"></i>
                         </div>
-                        <a href="#" class="small-box-footer">
-                          
-                        </a>
                     </div>
-                </div> -->
+                </div>
             </div>
-            <div class="col-md-12 mr-10 text-center">
-                <img src="{{ asset('img/alur-tte-new.png') }}" alt="" class="" width="70%" height="30%">
+            <div class="row">
+                <div class="card col-lg-6 col-6">
+                    <!-- <div class="card-header">
+                        <h3 class="card-title"><strong>{{ Auth::user()->pegawai->nama }}</strong></h3>
+                    </div> -->
+                    <div class="card-body">
+                        <canvas id="tte-sudah" width="200" height="100"></canvas>
+                    </div>
+                </div>
+                <div class="card col-lg-6 col-6">
+                    <!-- <div class="card-header">
+                        <h3 class="card-title"><strong>{{ Auth::user()->pegawai->nama }}</strong></h3>
+                    </div> -->
+                    <div class="card-body">
+                        <canvas id="tte-belum" width="200" height="100"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex flex-column justify-content-center align-items-center text-center" style="height: 100%;">
+                <button id="toggleImageBtn" class="btn btn-success mb-3" style="border-radius:25px;"><strong>ALUR PENGGUNAAN TTE</strong></button>
+                <img id="alurTteImage" src="{{ asset('img/alur-tte-new.png') }}" alt="Alur TTE" style="width: auto; height: auto; display: none;">
             </div>
         </div><!-- /.container-fluid -->
     </section>
     <br>
     <!-- /.content -->
 </div>
+@endsection
+
+@section('script')
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    const months = @json($months);
+    const dataBelum = @json($belum);
+    const dataSudah = @json($sudah);
+
+    const ctx = document.getElementById('tte-sudah').getContext('2d');
+    const cty = document.getElementById('tte-belum').getContext('2d');
+    new Chart(ctx, {
+        type: 'line', 
+        data: {
+            labels: months,
+            datasets: [
+                // {
+                //     label: 'BELUM',
+                //     data: dataBelum,
+                //     borderColor: 'rgba(54, 162, 235, 1)', 
+                //     backgroundColor: 'rgba(54, 162, 235, 0.2)', 
+                //     borderWidth: 2, 
+                //     fill: true, 
+                //     tension: 0.3, 
+                // },
+                {
+                    label: 'SUDAH',
+                    data: dataSudah,
+                    borderColor: 'rgba(75, 192, 192, 1)', 
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)', 
+                    borderWidth: 2, 
+                    fill: true, 
+                    tension: 0.3, 
+                },
+            ],
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top', 
+                },
+                title: {
+                    display: true,
+                    text: 'Grafik Jumlah Dokumen TTE Per Bulan (SUDAH)', 
+                },
+            },
+            scales: {
+                x: {
+                    beginAtZero: true,
+                },
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+    new Chart(cty, {
+        type: 'line', 
+        data: {
+            labels: months,
+            datasets: [
+                {
+                    label: 'BELUM',
+                    data: dataBelum,
+                    borderColor: 'rgba(54, 162, 235, 1)', 
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)', 
+                    borderWidth: 2, 
+                    fill: true, 
+                    tension: 0.3, 
+                },
+                // {
+                //     label: 'SUDAH',
+                //     data: dataSudah,
+                //     borderColor: 'rgba(75, 192, 192, 1)', 
+                //     backgroundColor: 'rgba(75, 192, 192, 0.2)', 
+                //     borderWidth: 2, 
+                //     fill: true, 
+                //     tension: 0.3, 
+                // },
+            ],
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top', 
+                },
+                title: {
+                    display: true,
+                    text: 'Grafik Jumlah Dokumen TTE Per Bulan (BELUM)', 
+                },
+            },
+            scales: {
+                x: {
+                    beginAtZero: true,
+                },
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+</script>
+<script>
+    document.getElementById('toggleImageBtn').addEventListener('click', function () {
+        const image = document.getElementById('alurTteImage');
+        const button = this;
+
+        if (image.style.display === 'none') {
+            image.style.display = 'block';
+            button.textContent = 'Sembunyikan Gambar';
+        } else {
+            image.style.display = 'none';
+            button.textContent = 'Tampilkan Gambar';
+        }
+    });
+</script>
 @endsection
