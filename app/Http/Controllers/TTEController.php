18e79836ccff72320d2ca286c748597b79622602
Rayandra Yala Pratama <rayandra.pratama@gmail.com>
2025-04-15 09:30:56 +0700

    Update TTEController.php

--- app/Http/Controllers/TTEController.php
+++ app/Http/Controllers/TTEController.php
@@ -561,7 +561,7 @@ class TTEController extends Controller
               CURLOPT_POSTFIELDS => $postfield,
               CURLOPT_HTTPHEADER => array(
                 'Content-Type: application/json',
-                'Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBsaWNhdGlvbiI6IjY3ZDhmYTU2OTY3ZThmMDAxMmEyOTIyZiIsImlhdCI6MTc0MjI3Njg1N30.PMgFwFHhKoHczwVlcdn0z1eak3Mix5VWQpG9etGUurYJZ9vvyVCAZMn3kZ4gHd0XXrfLp-_sLvE1Q8pm2DtlXfk13bDLeO7NgKDUKTCijGYMXF4GA1dfBDfDP8bUMpvn8cQi2H0fxcIxRZn594afoJnD_Sk0xv_LU7yUSmbTDxEaYSFTElGzvOno7pMqhNjdg7cbRBCiBKxzjZYWLb-c811YUxZ86WWTABImkrYyDT2DRcI-vajBC6s9de-UmvNrgGc-XojM1N_avAuzJXOvc9F6QBSePD73onotKg4k-EbgB0H2Bq4kUbDjPP5AZoH-F3pq3AZkyQYWyHHkmxAYbA'
+                'Authorization: eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBsaWNhdGlvbiI6IjY3ZDhmYTU2OTY3ZThmMDAxMmEyOTIyZiIsImlhdCI6MTc0MjI3Njg1N30.PMgFwFHhKoHczwVlcdn0z1eak3Mix5VWQpG9etGUurYJZ9vvyVCAZMn3kZ4gHd0XXrfLp-_sLvE1Q8pm2DtlXfk13bDLeO7NgKDUKTCijGYMXF4GA1dfBDfDP8bUMpvn8cQi2H0fxcIxRZn594afoJnD_Sk0xv_LU7yUSmbTDxEaYSFTElGzvOno7pMqhNjdg7cbRBCiBKxzjZYWLb-c811YUxZ86WWTABImkrYyDT2DRcI-vajBC6s9de-UmvNrgGc-XojM1N_avAuzJXOvc9F6QBSePD73onotKg4k-EbgB0H2Bq4kUbDjPP5AZoH-F3pq3AZkyQYWyHHkmxAYbA'
               ),
             ));
             
