709460f40e8668a931a8733077e9f4120f4b9514
Rayandra Yala Pratama <rayandra.pratama@gmail.com>
2025-08-19 09:00:19 +0700

    Update APITTEController.php
    perbaikan tanggal_upload

--- app/Http/Controllers/APITTEController.php
+++ app/Http/Controllers/APITTEController.php
@@ -170,7 +170,7 @@ class APITTEController extends Controller
                 //hapus file lama
                 unlink(storage_path('app/rekam-medis/' . $request->jenis_rm . '/' . $nama_file));
 
-                $tgl_upload = ManajemenTTE::pluck('tgl_upload')->where('no_rawat', '=', $request->no_rawat)->where('path', '=', $nama_file)->first();
+                $tgl_upload = ManajemenTTE::pluck('tanggal_upload')->where('no_rawat', '=', $request->no_rawat)->where('path', '=', $nama_file)->first();
 
                 try{
                     
